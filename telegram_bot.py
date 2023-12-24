b5e43d25c7af5b6747020f6c81e159797bd5e1a9
Aleksey <130661542+Alik90210@users.noreply.github.com>
2023-12-24 21:21:57 +0300

    Update telegram_bot.py
    add comments

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,18 +5,20 @@ from aiogram import Bot, types
 from aiogram.dispatcher.dispatcher import Dispatcher
 from aiogram.utils import executor
 
+# creating bot with aiogram library
 bot = Bot(token=tg_bot_token)
 dp = Dispatcher(bot)
 
-
+# waiting START command from user
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
     await message.reply("Привет! Вставь URL фото")
 
-
+# waiting URL from user
 @dp.message_handler()
 async def find_fox(message: types.Message):
 
+    # try to connect with Roboflow API
     try:
         rf = Roboflow(api_key=api_key)
         project = rf.workspace().project(project_id)
@@ -24,13 +26,15 @@ async def find_fox(message: types.Message):
 
         # infer on an image hosted elsewhere
         predictions = model.predict(message.text, hosted=True, confidence=confidence, overlap=iou_thresh).json()
-
+        
+    # print json answer in the chat bot
         await message.reply(predictions)
-
+        
+    # print Error in the chat bot, if we've catched error 
     except Exception as e:
         await message.reply(e)
         await message.reply("\U00002620 check URL \U00002620")
 
-
+# running bot
 if __name__ == '__main__':
     executor.start_polling(dp)
