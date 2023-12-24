4386ebeda1c3c6a07604eaa8a2a24bbc91b72e76
Aleksey <130661542+Alik90210@users.noreply.github.com>
2023-12-24 21:24:58 +0300

    Update fox_detect_v1.py
    add comments

--- fox_detect_v1.py
+++ fox_detect_v1.py
@@ -5,7 +5,10 @@ from roboflow import Roboflow
 #Paste private API
 #Contact me in Telegramm https://t.me/N9021010
 
-rf = Roboflow(api_key="")
+# creating Roboflow object and connecting to API
+rf = Roboflow(api_key="paste your API_KEY")
+
+# upload API parametrs
 project = rf.workspace("mipt23").project("fox_pic")
 dataset = project.version(2).download("yolov5")
 model = project.version(2).model
@@ -13,6 +16,7 @@ model = project.version(2).model
 #paste your pic with a fox
 img_url = " "
 
+# get json request from server
 predictions = model.predict(img_url, hosted=True, confidence=70, overlap=30).json()
 
 print(predictions)
