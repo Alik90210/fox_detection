6daebb0065a2ca68cf4eb0e1c0fbb7ea95d7b5af
Aleksey <130661542+Alik90210@users.noreply.github.com>
2023-12-28 13:56:27 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,20 +5,19 @@ from aiogram import Bot, types
 from aiogram.dispatcher.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-# creating bot with aiogram library
+
 bot = Bot(token=tg_bot_token)
 dp = Dispatcher(bot)
 
-# waiting START command from user
+
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
     await message.reply("Привет! Вставь URL фото")
 
-# waiting URL from user
+
 @dp.message_handler()
 async def find_fox(message: types.Message):
 
-    # try to connect with Roboflow API
     try:
         rf = Roboflow(api_key=api_key)
         project = rf.workspace().project(project_id)
@@ -26,15 +25,26 @@ async def find_fox(message: types.Message):
 
         # infer on an image hosted elsewhere
         predictions = model.predict(message.text, hosted=True, confidence=confidence, overlap=iou_thresh).json()
-        
-    # print json answer in the chat bot
-        await message.reply(predictions)
-        
-    # print Error in the chat bot, if we've catched error 
+
+        det_class = predictions['predictions'][0]['class']
+        confidence_pred = predictions['predictions'][0]['confidence']
+        height = predictions['predictions'][0]['height']
+        width = predictions['predictions'][0]['width']
+        x = predictions['predictions'][0]['x']
+        y = predictions['predictions'][0]['y']
+
+        await message.reply( f'class: {det_class}\n'
+                            f'the model s confidence: {round(confidence_pred*100)} procents\n'
+                            f'the height of the bounding box: {height}\n'
+                            f'the width of the bounding box: {width}\n'
+                            f'the horizontal center point of the detected object: {x}\n'
+                            f'the vertical center point of the detected object: {y}'
+                            )
+
     except Exception as e:
         await message.reply(e)
         await message.reply("\U00002620 check URL \U00002620")
 
-# running bot
+
 if __name__ == '__main__':
     executor.start_polling(dp)
